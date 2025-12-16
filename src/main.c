cefeeb7522ea351c0d11e528f393fdd0c98bab29
LozanoPiton <lozanosorannol@gmail.com>
2025-12-15 22:55:48 -0400

    fix: improve character movement and adjust text position in game

--- src/main.c
+++ src/main.c
@@ -24,10 +24,21 @@ int main ()
 
 	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
+
+
+    // 1. DEFINIR DATOS DEL PERSONAJE
+    Vector2 move = { (float)800/2, (float)600/2 };
+    float speed = 5.0f;
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
+		if (IsKeyDown(KEY_RIGHT)) move.x += speed;
+        if (IsKeyDown(KEY_LEFT)) move.x -= speed;
+
+        // Movimiento vertical
+        if (IsKeyDown(KEY_DOWN)) move.y += speed; // Recuerda: +Y es abajo
+        if (IsKeyDown(KEY_UP)) move.y -= speed;
 		// drawing
 		BeginDrawing();
 
@@ -35,10 +46,10 @@ int main ()
 		ClearBackground(WHITE);
 
 		// draw some text using the default font
-		DrawText("Sopas", 200,200,20, BLACK);
+		DrawText("Sopas", 200,40,20, BLACK);
 
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		DrawTexture(wabbit, move, WHITE);
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
